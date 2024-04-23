4a23f14c66a417f989088e7f22759dd6d94f16c2
medfahmy <medfahmy99@gmail.com>
2024-04-23 17:06:58 +0100

    pipe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{stdin, stdout, Write};
-use std::process::Command;
+use std::path::Path;
+use std::process::{Command, Child, Stdio};
 
 fn main() {
     loop {
@@ -8,10 +9,57 @@ fn main() {
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
-        let command = input.trim();
 
-        let mut child = Command::new(command).spawn().unwrap();
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        child.wait().unwrap();
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+
+            let cmd = parts.next().unwrap();
+            let args: Vec<&str> = parts.collect();
+
+            match cmd {
+                "exit" => return,
+                "cd" => {
+                    if let Err(e) =
+                        std::env::set_current_dir(&Path::new(args.first().unwrap_or(&"/")))
+                    {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                }
+                cmd => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => previous_command = Some(output),
+                        Err(err) => {
+                            previous_command = None;
+                            eprintln!("{}", err);
+                        }
+                    }
+                }
+            }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            final_command.wait();
+        }
     }
 }
